38c7b8a5a5536f8b14d6816c9eba8ccf4fbd70c8
Joseph Developer Doan <developer@formcircles.com>
2026-03-16 21:58:58 -0700

    FCTEST-57: Implement enterprise Playwright test framework folder architecture

---
+++ framework/api_client.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import APIRequestContext
+from framework.config import BASE_URL
+
+class APIClient:
+
+    def __init__(self, request: APIRequestContext):
+        self.request = request
+
+    def get_devices(self):
+        return self.request.get(f"{BASE_URL}/devices")
